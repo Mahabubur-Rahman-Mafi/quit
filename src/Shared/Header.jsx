10f317fa7a046a970da6e33036258091e64d5ad3
Mahabubur Rahman <mrmafi32@gmail.com>
2022-10-25 13:46:59 +0600

    header done

--- src/Shared/Header.jsx
+++ src/Shared/Header.jsx
@@ -1,10 +1,58 @@
 import React from 'react';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import img from '../../src/Assest/IMG_20210829_154357-removebg-preview (1).png'
+import { MdDarkMode } from "react-icons/md";
+import { BsSun } from "react-icons/bs";
+import logo from '../Assest/logo.png'
+import './Shared.css'
+import { Link, NavLink } from 'react-router-dom';
+
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is header</h1>
-        </div>
+      <Navbar collapseOnSelect expand="lg" variant="light" bg="transparent">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src={logo}
+              width="120"
+              height="40"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto header-menu-item">
+              <NavLink to="/home">Home</NavLink>
+              <NavLink to="/courses">Courses</NavLink>
+              <NavLink to="/blog">Blog</NavLink>
+              <NavLink to="/fqa">FAQ</NavLink>
+            </Nav>
+            <Nav>
+              <ButtonGroup aria-label="Basic example">
+                <Button variant="outline-dark" className="px-3">
+                  <BsSun className="fs-6"></BsSun>
+                </Button>
+                <Button variant="outline-dark" className="px-3">
+                  <MdDarkMode className="fs-6"></MdDarkMode>
+                </Button>
+              </ButtonGroup>
+
+              <div>
+                <img src={img} alt="" className="user-image-profile ms-3"></img>
+                <Link to='/log-in'>
+                  <Button className="ms-3">Log In</Button>
+                </Link>
+              </div>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     );
 };
 
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,33 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Blog from "../pages/Blog/Blog";
+import Courses from "../pages/Courses/Courses";
+import Home from "../pages/Home/Home";
 
 
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element:<Main></Main>
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element:<Home></Home>
+            },
+            {
+                path: '/home',
+                element:<Home></Home>
+            },
+            {
+                path: '/courses',
+                element:<Courses></Courses>
+            },
+            {
+                path: 'blog',
+                element:<Blog></Blog>
+            },
+
+        ]
     }
 ])
